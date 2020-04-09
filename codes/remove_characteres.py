847f9221b53a480b5e2b76fd79e03ee581a81e25
raffoliveira <rafaelollywer@gmail.com>
2020-04-09 20:07:06 -0300

    Coding remove repeated characteres

--- codes/remove_characteres.py
+++ codes/remove_characteres.py
@@ -6,15 +6,31 @@ from nltk.tokenize import word_tokenize
 # re = regular expression operations 
 
 def remove_URL(text):
+
 	return re.sub(r'http\S+', '', text) #re.sub(pattern, repl, string)
 
+
 def remove_simbols(text):
+
 	return re.sub(r'[^\w\s]','',text)
 
+
 def remove_numbers(text):
+
 	return re.sub(r'\d+','',text)
 
+
+def remove_repeated_letters(text):
+
+	text_tokens = word_tokenize(text)
+
+	text_processed = [''.join(sorted(set(i), key = i.index)) for i in text_tokens]
+
+	return text_processed
+
+
 def remove_punctuation(text):
+
 	new_words = []
 	for word in text:
 		new_word = re.sub(r'[^\w\s]', '', word)
@@ -22,15 +38,15 @@ def remove_punctuation(text):
 			new_words.append(new_word)
 	return new_words
 
+
 def replace_stop_words(text):
 		 
-	stop_words = set(stopwords.words('portuguese')) 
-	  
+	stop_words = set(stopwords.words('portuguese')) 	  
 	word_tokens = word_tokenize(text) 
-
 	filtered_sentence = [w for w in word_tokens if not w in stop_words] 
-	
+
 	return filtered_sentence
+
 
 def main():
 
@@ -40,6 +56,7 @@ def main():
 
 	new_text = remove_URL(original_text)
 	new_text = new_text.lower()
+	# new_text = remove_repeated_letters(new_text)
 	new_text = remove_numbers(new_text)
 	new_text = replace_stop_words(new_text)
 	new_text = remove_punctuation(new_text)
