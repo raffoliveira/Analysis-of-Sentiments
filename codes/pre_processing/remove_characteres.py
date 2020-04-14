2c3905412bd77252a85629619923a8d004ceb3c0
raffoliveira <rafaelollywer@gmail.com>
2020-04-14 16:30:10 -0300

    Testing remove_characteres

--- codes/pre_processing/remove_characteres.py
+++ codes/pre_processing/remove_characteres.py
@@ -1,41 +1,19 @@
-import json, re
+import json, re, gzip
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
 from nltk.tokenize.treebank import TreebankWordDetokenizer
-
 
 # ------------------------------------------------------------------------------
 
 def remove_URL(text):
 
-	return re.sub(r'http\S+', '', text) #re.sub(pattern, repl, string)
+	return re.sub(r'http\S+', '', text)
 
 # ------------------------------------------------------------------------------
 
 def remove_numbers(text):
 
 	return re.sub(r'\d+','',text)
-
-# ------------------------------------------------------------------------------
-
-def remove_repeated_letters(text):
-
-	text_tokens = word_tokenize(text)
-	new_word = 'text'
-	removed_text = []
-
-	for each_word in text_tokens:
-		if len(each_word) > 1 and (each_word[-1] == each_word[-2]):			
-			for i in range(1, len(each_word)):
-				if each_word[-i] == each_word[-(i+1)]:
-					new_word = each_word[:-i]
-				else:
-					break				
-			removed_text.append(new_word)
-		else:
-			removed_text.append(each_word)
-
-	return TreebankWordDetokenizer().detokenize(removed_text)
 
 # ------------------------------------------------------------------------------
 
@@ -64,21 +42,92 @@ def remove_stop_words(text):
 
 # ------------------------------------------------------------------------------
 
+def remove_repeated_letters(text):
+
+	text_tokens = word_tokenize(text)
+	new_word = 'text'
+	removed_text = []
+
+	for each_word in text_tokens:
+		if len(each_word) > 1 and (each_word[-1] == each_word[-2]):			
+			for i in range(1, len(each_word)):
+				if each_word[-i] == each_word[-(i+1)]:
+					new_word = each_word[:-i]
+				else:
+					break				
+			removed_text.append(new_word)
+		else:
+			removed_text.append(each_word)
+
+	return TreebankWordDetokenizer().detokenize(removed_text)
+
+# ------------------------------------------------------------------------------
+
+def pre_processing(text):
+
+	text = text.lower()
+	text = remove_hashtag_mentions(text)
+	text = remove_URL(text)
+	text = remove_numbers(text)
+	text = remove_punctuation_symbols(text)
+	text = remove_stop_words(text)		
+	text = remove_repeated_letters(text)
+
+	return text
+
+# ------------------------------------------------------------------------------
+
+def writeInFile(file, content):
+
+	file.write('%s\n' % json.dumps(content))
+
+# ------------------------------------------------------------------------------
+
+def processing(profile_id):
+
+	destination_path = '/home/rafael/TCC_II/coleta_TCC_II/coletas'
+	months = ['agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+	file_part1 = 'comments/comments_total.json'
+	file_part2 = 'comments/comments_total_processed.json'
+	
+	for id_months in months:
+		file_read = open('%s/%s/%s/%s' % (destination_path, profile_id, id_months, file_part1),'rt')
+		file_write = open('%s/%s/%s/%s' % (destination_path, profile_id, id_months, file_part2),'wt')
+
+		for line in file_read:
+
+			comments_processed = []
+			id_post = json.loads(line)['id_post']
+			comments_total = json.loads(line)['comments']
+
+			for each_comment in comments_total:
+
+				processed_text = pre_processing(each_comment['message'])
+
+				content_comment = {'created_time': each_comment['created_time'],\
+						   'id': each_comment['id'],\
+						   'message': processed_text,\
+						   'comment_count': each_comment['comment_count']}
+
+				comments_processed.append(content_comment)
+
+			content_final = {'id_post': id_post, 'comments': comments_processed}
+			writeInFile(file_write, content_final)
+
+		file_read.close()
+		file_write.close()
+
+# ------------------------------------------------------------------------------
+
 def main():
+	
+	profiles = ['bolsonaro', 'haddad']	
 
-	original_text = 'Falaaaa a porque aeeeee #falar, b4lz? bom #dmaissss esta vaca e @fald na https://fdfjdo.vdo.com fldlfmlsdsaaaaaa ae mannnooooooo'
-	print('Original text: %s' % original_text)
-	new_text = original_text
+	# for id_profile in profiles:
 
-	new_text = new_text.lower()
-	new_text = remove_hashtag_mentions(new_text)
-	new_text = remove_URL(original_text)
-	new_text = remove_numbers(new_text)
-	new_text = remove_punctuation_symbols(new_text)
-	new_text = remove_stop_words(new_text)		
-	new_text = remove_repeated_letters(new_text)
+	processing(profiles[1])
 
-	print('\nProcessed text: %s' % new_text)
+
 
 if __name__ == '__main__':
 	main()
